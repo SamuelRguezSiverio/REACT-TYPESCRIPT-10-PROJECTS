ad4bf74292c5b27ed8a5360743b6c75dcf8d409b
SamuelRguezSiverio <samuelsrs89@gmail.com>
2024-08-26 20:04:44 +0100

    Working on input events change

--- 03_calorieTracker/src/components/Form.tsx
+++ 03_calorieTracker/src/components/Form.tsx
@@ -1,6 +1,25 @@
+import { useState } from 'react'
 import { categories } from '../data/categories'
+import { Activity } from '../types'
 
 export default function Form() {
+  const [activity, setActivity] = useState<Activity>({
+    category: 1,
+    name: '',
+    calories: 0,
+  })
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLSelectElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumberField = ['category', 'calories'].includes(e.target.id)
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
+    })
+  }
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
@@ -10,6 +29,8 @@ export default function Form() {
         <select
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
+          value={activity.category}
+          onChange={handleChange}
         >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -20,14 +41,16 @@ export default function Form() {
       </div>
 
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad:
         </label>
         <input
-          id="activity"
+          id="name"
           type="text"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta..."
+          value={activity.name}
+          onChange={handleChange}
         ></input>
       </div>
 
@@ -40,6 +63,8 @@ export default function Form() {
           type="text"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Calorías ej. 300 o 500"
+          value={activity.calories}
+          onChange={handleChange}
         ></input>
       </div>
 
--- 03_calorieTracker/src/types/index.ts
+++ 03_calorieTracker/src/types/index.ts
@@ -2,3 +2,9 @@ export type Category = {
   id: number
   name: string
 }
+
+export type Activity = {
+  category: number
+  name: string
+  calories: number
+}
