7dc42adcf0a0bc6eedf4c302506c7b603633d302
SamuelRguezSiverio <samuelsrs89@gmail.com>
2024-09-06 17:57:24 +0100

    Context working properly

--- 06_expenseControl_contextApi/src/App.tsx
+++ 06_expenseControl_contextApi/src/App.tsx
@@ -1,14 +1,17 @@
 import BudgetForm from './components/BudgetForm'
+import { useBudget } from './hooks/useBudget'
 
 export default function App() {
+  const { state, dispatch } = useBudget()
+  console.log(state)
   return (
     <>
-      <header className="bg-teal-600 py-8 max-h72">
-        <h1 className="uppercase text-center font-black text-4xl text-white">
+      <header className="py-8 bg-teal-600 max-h72">
+        <h1 className="text-4xl font-black text-center text-white uppercase">
           Planificador de Gastos
         </h1>
       </header>
-      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+      <div className="max-w-3xl p-10 mx-auto mt-10 bg-white rounded-lg shadow-lg">
         <BudgetForm />
       </div>
     </>
--- 06_expenseControl_contextApi/src/main.tsx
+++ 06_expenseControl_contextApi/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BudgetProvider } from './ context/BudgetContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BudgetProvider>
+      <App />
+    </BudgetProvider>
+  </StrictMode>
 )
